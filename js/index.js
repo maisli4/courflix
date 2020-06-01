18d084af1edab115160b729467aa63178999c72a
maisli <maislipczuk@gmail.com>
2020-06-01 19:00:14 -0300

    versión final

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-const menu = document.querySelector(".js-hamburger")
+const menuButton = document.querySelector(".js-hamburger")
+const nav = document.querySelector(".js-nav")
+
+menuButton.onclick = function () {
+    if (nav.classList.contains("open")) {
+        nav.classList.remove("open")
+    } else {
+        nav.classList.add("open")
+    }
+}
